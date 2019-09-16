976e0cd8e1dacabf4868c810229ebd1183461170
mmanchen <mmanchen@u.rochester.edu>
2019-09-16 16:33:03 -0400

    Update nav.php

--- inc/nav.php
+++ inc/nav.php
@@ -1,6 +1,6 @@
 	<nav class="home_nav">
 		<ul>
-			<li><a href="index.php">Me</a></li>
+			<li><a href="index.php">Home</a></li>
 			<li><a href="clara.php">Clara</a></li>
 			<li><a href="andrew.php">Andrew</a></li>
 			<li><a href="ana.php">Ana</a></li>
