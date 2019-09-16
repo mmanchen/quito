0a1a23dea899a4bb17ba937aca3314308b43c116
mmanchen <mmanchen@u.rochester.edu>
2019-09-16 16:37:38 -0400

    Update navn.php

--- inc/navn.php
+++ inc/navn.php
@@ -1,7 +1,7 @@
 
 	<nav>
 		<ul>
-			<li><a <?php if($current == 'home') {echo 'class="is-current"';} ?> href="andrew.php">Home</a></li>
+			<li><a <?php if($current == 'home') {echo 'class="is-current"';} ?> href="andrew.php">Me</a></li>
 			<li><a <?php if($current == 'earlydays') {echo 'class="is-current"';} ?> href="early_days.php">Early Days</a></li>
 			<li><a <?php if($current == 'college') {echo 'class="is-current"';} ?> href="college.php">College Life</a><li>
 			<li><a <?php if($current == 'future') {echo 'class="is-current"';} ?> href="future.php">Future Plans</a></li>
