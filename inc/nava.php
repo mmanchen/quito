21815da243efc3d42dbe1000765aafa8d7d3ab79
mmanchen <mmanchen@u.rochester.edu>
2019-09-16 16:31:30 -0400

    Update nava.php

--- inc/nava.php
+++ inc/nava.php
@@ -1,6 +1,6 @@
 	<nav>
 		<ul>
-			<li><a href="ana.php">Home</a></li>
+			<li><a href="ana.php">Me</a></li>
 			<li><a href="journey.php">Journey</a></li>
 			<li><a href="today.php">Today</a></li>
 			<li><a href="tomorrow.php">Tomorrow</a></li>
