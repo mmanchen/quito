ee5295d55f24ce594d111f88288227922991373e
mmanchen <mmanchen@u.rochester.edu>
2019-09-18 11:12:29 -0400

    Create andrew-early.php

---
+++ andrew-early.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Andrew Thankson </title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<script src="js/menu-highlighter.js"></script>
+</head>
+<body class= "container">
+
+
+	<header>
+		<a href= "index.php">
+			<h1> Andrew Thankson </h1>
+			<img src= "images/andrew.jpeg" alt="Andrew">
+		</a>
+	</header>
+	<?php include "inc/nav-andrew.php"; ?>
+	<?php include "inc/nav.php"; ?>
+
+	<aside>
+		<ul>
+			<li>Computer Science Student, University of Rochester</li>
+			<li>Freelance Web Developer</li>
+			<li>Entrepreneur</li>
+		</ul>
+	</aside>
+
+
+	<main>
+		<h1> Early Days </h1>
+
+		<p>I was born in a small town in the Eastern Region of Ghana called Aburi. I grew up in this town and attended most of my basic education schools here. I graduated junior high school in 2014 and went about 30 minutes away to a boarding school for my high school education. In high school I studied General Science which was a combination of core subjects English, Social Studies, Mathematics and Integrated Science with my electives being Physics, Chemistry, Biology and Further Mathematics. In high school I also heard about programming and took quite an interest in it. I also loved social activism and being involved in driving social progress by helping out in whatever ways that I could. I loved volunteering, I still do, and anything that had to do with social advancement. After graduating high school, I was faced with the decision of studying in home country or taking an opportunity that was presented to me to be mentored in applying to schools outside my home country, Ghana. I decided to give the latter choice a shot. I took my SATs, wrote lots of essays and ended up applying early decision to the University of Rochester. I got in right away accepted to go there as it was one of the top schools I was looking to attend.</p>
+
+	</main>
+
+
+	<?php include "inc/scripts.php";?>
+	<footer> <strong> CSC 174: Advanced Front-end Web Design and Development </strong></footer>
+	
+
+</body></html>
