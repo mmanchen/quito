22f1af550f37a943fd97a2fa321e90b563e1b106
mmanchen <mmanchen@u.rochester.edu>
2019-09-18 11:12:40 -0400

    Create clara-favorite.php

---
+++ clara-favorite.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Clara Martinez Rubio </title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<script src="js/menu-highlighter.js"></script>
+</head>
+<body class= "container">
+
+
+	<header>
+		<a href= "index.php">
+			<h1> Pancho Peach </h1>
+			<img src= "images/pancho.jpg" alt="Pancho">
+		</a>
+	</header>
+	<?php include "inc/nav-clara.php"; ?>
+	<?php include "inc/nav.php"; ?>
+
+	<aside>
+		<ul>
+			<li>Trend setter</li>
+			<li>Bandana model</li>
+			<li>Instagram influencer</li>	
+		</ul>
+	</aside>
+
+
+	<main>
+		<h1> Favorite Things </h1>
+
+		<p>My favorite morning hobby is peeing in as many trees as I can during my river side morning walk. This takes a lot of careful planning because walks aren't the same length everyday so, I have to calculate precisely. Afternoons are fun because gets back from school. Then, we get to play ball and tug of war until I get tired and then I usually get a nice good boy massage. Weekends are the best!!! Mom works at the Farmer's Market and I always get a surprise!! The other farmer's always ask about me and always give mom bones, meat, treats, or some fun toy. But if I had to pick nights are my favorite. I get a Dentastix while mom brushes her teeth so, one could say we brush our teeth at the same time. Then she gets in bed and I impatiently dance at the bottom until she fixes the blankets and pads the bed as a signal for me to jump up. Then I crowl into a little ball next to her until she turns off the light.</p>
+
+	</main>
+
+
+	<?php include "inc/scripts.php";?>
+	<footer> <strong> CSC 174: Advanced Front-end Web Design and Development </strong></footer>
+	
+
+</body></html>
