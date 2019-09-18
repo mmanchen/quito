15d7fd025b3962455e6cac1dbae73aed0d7a760d
mmanchen <mmanchen@u.rochester.edu>
2019-09-18 11:13:14 -0400

    Create nav-clara.php

---
+++ inc/nav-clara.php
@@ -0,0 +1,8 @@
+	<nav class="sub-menus">
+	<ul>
+			<li><a href="clara-index.php">Me</a></li>
+			<li><a href="clara-favorite.php">Favorite Things</a></li>
+			<li><a href="clara-lifestyle.php">Lifestyle</a></li>
+			<li><a href="clara-past.php">Past</a></li>
+	</ul>	
+	</nav>
