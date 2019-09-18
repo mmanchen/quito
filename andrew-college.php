85294727ab0f27b48dcc68ee1dd04402cebb18b9
mmanchen <mmanchen@u.rochester.edu>
2019-09-18 11:12:27 -0400

    Create andrew-college.php

---
+++ andrew-college.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Andrew Thankson </title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<script src="js/menu-highlighter.js"></script>
+</head>
+<body class= "container">
+
+
+	<header>
+		<a href= "index.php">
+			<h1> Andrew Thankson </h1>
+			<img src= "images/andrew.jpeg" alt="Andrew">
+		</a>
+	</header>
+	<?php include "inc/nav-andrew.php"; ?>
+	<?php include "inc/nav.php"; ?>
+
+	<aside>
+		<ul>
+			<li>Computer Science Student, University of Rochester</li>
+			<li>Freelance Web Developer</li>
+			<li>Entrepreneur</li>
+		</ul>
+	</aside>
+
+
+	<main>
+		<h1> College Life </h1>
+
+		<p>I arrived to College the Summer before the start of my first year to participate in an early connections program called the Summer Start Program which was geered at helping students learn more about the University's campus, make early connections and also get a feel of college classes. I took classes in Engineering, Writing and also in College Success. It was during this period that I decided to switch my inteded major from Electrical and Computer Engineering to Computer Science. During my first year, I was an EcoRep (first year students who promote environmental sustainability in first year housing). I did a lot of volunteer work through this program, which included, developing a website for Grow Green, an off campus garden. I also was involved with still managing my non-profit organization alongside being involved in campus clubs such as the National Society of Black Engineers, the Pan-African Student Association and the Philosophy Council. I was also an eboard member of my scholarship's community engagement committee. I also worked as an Office Assistant in the Hajim School of Engineering Dean's Office. In my second year, I am an International Student Mentor, a first year fellow (academic counselor for first years), and also still work as an Office Assistant in the Hajim School of Engineering Dean's Office and as a program assistant in the David T. Kearns Center. I hope to graduate in 2022 with a bachelor of Science degree in Computer Science and  double minors in Philosophy and Psychology.</p>
+
+	</main>
+
+
+	<?php include "inc/scripts.php";?>
+	<footer> <strong> CSC 174: Advanced Front-end Web Design and Development </strong></footer>
+	
+
+</body></html>
