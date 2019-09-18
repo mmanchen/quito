41021c4edb7717e88ccb18b2e3e0251ca1987b1e
mmanchen <mmanchen@u.rochester.edu>
2019-09-18 11:13:10 -0400

    Create nav-ana.php

---
+++ inc/nav-ana.php
@@ -0,0 +1,16 @@
+	<nav class="sub-menus">
+	<ul>
+		<li>
+			<a href="ana-index.php"> Me </a>
+		</li>
+		<li>
+			<a href="ana-journey.php">Journey</a>
+		</li>
+		<li>
+			<a href="ana-today.php">Today</a>
+		</li>
+		<li>
+			<a href="ana-tomorrow.php"> Tomorrow </a>
+		</li>
+	</ul>	
+	</nav>
