0743a8aa31b5e4a3a515751e41f79c82ebe79c39
mmanchen <mmanchen@u.rochester.edu>
2019-09-18 11:12:23 -0400

    Create ana-tomorrow.php

---
+++ ana-tomorrow.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Ana Acuña </title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<script src="js/menu-highlighter.js"></script>
+</head>
+<body class= "container">
+
+
+	<header>
+		<a href= "index.php">
+			<h1> Ana Sofía Acuña </h1>
+			<img src= "images/ana.jpg" alt="Ana">
+		</a>
+	</header>
+	<?php include "inc/nav-ana.php"; ?>
+	<?php include "inc/nav.php"; ?>
+
+	<aside>
+		<ul>
+			<li>Marketing student flirting with Computer Science</li>
+			<li>Cat lover</li>
+			<li>Looking for the perfect first tattoo</li>
+		</ul>
+	</aside>
+
+
+	<main>
+		<h1> Tomorrow </h1>
+
+		<p>I’m not quite sure what the future holds, but I’m excited! This summer I worked at Allstate, where I met a lot of great people but I wasn’t in the department that was right for me. Thankfully, I received and accepted an offer from Allstate for their rotational program. I’m really looking forward to that! I’m not sure where I’ll be placed, but I’m looking as that as part of the fun and as part of the experience. After the rotational program, I might go to law school, but I haven’t made up my mind about that. I might also want to be a User Experience Researcher. I clearly have a lot to figure out, but I have time!</p>
+
+	</main>
+
+
+	<?php include "inc/scripts.php";?>
+	<footer> <strong> CSC 174: Advanced Front-end Web Design and Development </strong></footer>
+	
+
+</body></html>
