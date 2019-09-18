f4f1bae1ab793e50537d5cb6a4062efffc263ed9
mmanchen <mmanchen@u.rochester.edu>
2019-09-18 11:12:19 -0400

    Create ana-journey.php

---
+++ ana-journey.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Ana Acuña </title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<script src="js/menu-highlighter.js"></script>
+</head>
+<body class= "container">
+
+
+	<header>
+		<a href= "index.php">
+			<h1> Ana Sofía Acuña </h1>
+			<img src= "images/ana.jpg" alt="Ana">
+		</a>
+	</header>
+	<?php include "inc/nav-ana.php"; ?>
+	<?php include "inc/nav.php"; ?>
+
+	<aside>
+		<ul>
+			<li>Marketing student flirting with Computer Science</li>
+			<li>Cat lover</li>
+			<li>Looking for the perfect first tattoo</li>
+		</ul>
+	</aside>
+
+
+	<main>
+		<h1> Journey </h1>
+
+		<p>As much as I love my hometown, I followed my brother’s footsteps and went to school far from home for college. While my brother chose to study in the Midwest, I chose the Northeast which is just as cold. Adjusting to college was hard because I wasn’t quite sure what I wanted to study, although I pretended I was going to be a hardcore business major. After some experimenting in Data Science and Epidemiology, I decided I indeed want to study Business, specifically Marketing. I really liked my Statistics courses, but I liked getting creative with a group of peers more than anything. Marketing just feels like the best fit right now! I’m also minoring in Computer Science because it sparked my interest in my journey to figuring out my interests, but not enough to major in it. Besides, I would rather dabble in different CS courses and have fun with it than overstress about it since it’s not where my passions lie. </p>
+
+	</main>
+
+
+	<?php include "inc/scripts.php";?>
+	<footer> <strong> CSC 174: Advanced Front-end Web Design and Development </strong></footer>
+	
+
+</body></html>
