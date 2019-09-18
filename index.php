22108175bc1060c3ae7099934cc11397a7f5dfdd
mmanchen <mmanchen@u.rochester.edu>
2019-09-18 11:13:28 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Team Barcelona </title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<script src="js/menu-highlighter.js"></script>
+</head>
+<body class= "container">
 
-<?php include "inc/html-top.php"; ?>
+
+	<header>
+		<a href= "index.php">
+			<h1> Barcelona </h1>
+			<img src= "images/barcelona-photo.jpg" alt="city">
+			
+		</a>
+	</header>
 
 <?php include "inc/nav.php"; ?>
 
-	<!-- <a href = "folder 2/lab01/index.php" >Clara</a>
-	<a href = "file 1/index.php">Ana</a>
-	<a href = "andrew/index.php">Andrew</a> -->
-</html>
+	<aside>
+		<ul>
+			<li>City in Spain</li>
+			<li>Gaudi Architecture</li>
+			<li>Historic and Trendy</li>
+		</ul>
+	</aside>
 
 
+	<main>
+		<p>Everyone runs to Barcelona today. Out of all the cities in the world, what makes Barcelona special? It is the capital and largest city of the autonomous community of Catalonia. Trip through Geography class... What, there is an anonymous area in Spain? Spain comprises of a couple of independent states. Catalonia, the state in which Barcelona rests, is one of the most vital locations in Spain because of its neighbors. It neighbours France and Andorra to the North, Mediterraenan Sea to the East, Aragon to the West and Valencia to the South. Do they speak English? Get your facts right. The official languages in Barcelona are Catalan, Spanish and the Aranese dialect of Occitan. What is Barcelona known for? The Sagrada Familia! This monument has taken longer to build than the Egyptian Pyramids. The construction began in 1882 and is planned to be completed in 2026. If you don’t know the city then you know Barcelona FC, one of the best football teams in the world thanks to Lionel Messi. Prepare your belly for Barcelona has one of the most delicious cuisines in the world. Paella, a combination of rice and seafood is quite the delicacy. ‘La bomba’, also is another dish inspired by grenades used in the Spanish Civil War. This food bomb is composed of potato, garlic and a rich spicy red sauce. Set you mouth ablaze! You can’t leave Barcelona without the taste of Crema Catalana, a creamy vanilla custard that will make you fall in love with Barcelona for ever! All roads lead to Barcelona!!!</p>
+	</main>
+	
+	<?php include "inc/scripts.php";?>
+	<footer> <strong> CSC 174: Advanced Front-end Web Design and Development </strong></footer>
+	
 
+</body></html>
 
