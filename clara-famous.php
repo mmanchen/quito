1d8f0980413456cf5b773f4a6c3b4aa37518eb12
mmanchen <mmanchen@u.rochester.edu>
2019-09-18 11:12:38 -0400

    Create clara-famous.php

---
+++ clara-famous.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Clara Martinez Rubio </title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<script src="js/menu-highlighter.js"></script>
+</head>
+<body class= "container">
+
+
+	<header>
+		<a href= "index.php">
+			<h1> Pancho Peach </h1>
+			<img src= "images/pancho.jpg" alt="Pancho">
+		</a>
+	</header>
+	<?php include "inc/nav-clara.php"; ?>
+	<?php include "inc/nav.php"; ?>
+
+	<aside>
+		<ul>
+			<li>Trend setter</li>
+			<li>Bandana model</li>
+			<li>Instagram influencer</li>	
+		</ul>
+	</aside>
+
+
+	<main>
+		<h1> Famous Lifestyle </h1>
+
+		<p>The thing about me is that I'm funny & fabulous! I have a big personality, and according to everyone that meets me, I am constantly doing silly things. I like to fight fruits and vegetables, do yoga in the mornings, and sleep like a weird T-Rex at night. So, as you can imagine, that led to everyone that meets me being obsessed with me. One thing led to another one, and I ended up being Insta famous. Now I have over 50 different bandanas that I like to model and change every week. I will say tho, the fashion life is hard sometimes.</p>
+
+	</main>
+
+
+	<?php include "inc/scripts.php";?>
+	<footer> <strong> CSC 174: Advanced Front-end Web Design and Development </strong></footer>
+	
+
+</body></html>
