34c665cd2a14e7aa0a80d591644f2c3811177345
mmanchen <mmanchen@u.rochester.edu>
2019-09-17 21:51:22 -0400

    Update navc.php

--- inc/navc.php
+++ inc/navc.php
@@ -1,8 +1,8 @@
 	<nav>
 		<ul>
 			<li><a href="clara.php">Me</a></li>
-			<li><a href="tour.php">Before my forever home</a></li>
-			<li><a href="famous.php">About my famous life-style</a></li>
-			<li><a href="hobbies.php">My hobbies</a></li>
+			<li><a href="tour.php">Past</a></li>
+			<li><a href="famous.php">Life-Style</a></li>
+			<li><a href="hobbies.php">Hobbies</a></li>
 		</ul>	
 	</nav>
