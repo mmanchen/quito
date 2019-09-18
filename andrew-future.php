6b514e697892c6bb2cf950fa30fb9f729cadb9f1
mmanchen <mmanchen@u.rochester.edu>
2019-09-18 11:12:32 -0400

    Create andrew-future.php

---
+++ andrew-future.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Andrew Thankson </title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<script src="js/menu-highlighter.js"></script>
+</head>
+<body class= "container">
+
+
+	<header>
+		<a href= "index.php">
+			<h1> Andrew Thankson </h1>
+			<img src= "images/andrew.jpeg" alt="Andrew">
+		</a>
+	</header>
+	<?php include "inc/nav-andrew.php"; ?>
+	<?php include "inc/nav.php"; ?>
+
+	<aside>
+		<ul>
+			<li>Computer Science Student, University of Rochester</li>
+			<li>Freelance Web Developer</li>
+			<li>Entrepreneur</li>
+		</ul>
+	</aside>
+
+
+	<main>
+		<h1> Future Plans </h1>
+
+		<p>My future plans as of now are totally uncertain. I am thinking of graduating from college with a CS degree and working in industry for a bit as my first and default option. After that, I may want to return to school to get my masters or probably a pHD if I'm interested enough at that time. If I don't get a job however, I think I will go straight to graduate school to try and get my masters while working on securing a job during those years. I will also consider research as a possible career track and if I find it interesting enough, I may do a phd and become a faculty member at a University and do research.</p>
+
+	</main>
+
+
+	<?php include "inc/scripts.php";?>
+	<footer> <strong> CSC 174: Advanced Front-end Web Design and Development </strong></footer>
+	
+
+</body></html>
