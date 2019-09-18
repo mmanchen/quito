d990fca2d7dc7229a95d314f05a11496e6cce26a
mmanchen <mmanchen@u.rochester.edu>
2019-09-18 11:12:42 -0400

    Create clara-index.php

---
+++ clara-index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Clara Martinez Rubio </title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<script src="js/menu-highlighter.js"></script>
+</head>
+<body class= "container">
+
+
+	<header>
+		<a href= "index.php">
+			<h1> Pancho Peach </h1>
+			<img src= "images/pancho.jpg" alt="Pancho">
+		</a>
+	</header>
+	<?php include "inc/nav-clara.php"; ?>
+	<?php include "inc/nav.php"; ?>
+
+	<aside>
+		<ul>
+			<li>Trend setter</li>
+			<li>Bandana model</li>
+			<li>Instagram influencer</li>	
+		</ul>
+	</aside>
+
+
+	<main>
+		<p>Hi you guys! My name's Pancho I am a 5 year old corgi, beagle, and bulldog mix. I am originally from Virginia and I was brought to Rochester by a rescue shelter association. 
+			I recently discovered I like fighting frozen brocoli, and I constantly try to catch flys around the house. 
+			Because of my past homes I am scared of a lot of things, and I often bark at statues, benches, insects, and the vacuum cleaner. 
+ </p>
+	</main>
+
+
+	<?php include "inc/scripts.php";?>
+	<footer> <strong> CSC 174: Advanced Front-end Web Design and Development </strong></footer>
+	
+
+</body></html>
