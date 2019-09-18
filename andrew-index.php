22e9d77bb597d9982f437d89433e7666f228ff5c
mmanchen <mmanchen@u.rochester.edu>
2019-09-18 11:12:34 -0400

    Create andrew-index.php

---
+++ andrew-index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Andrew Thankson </title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<script src="js/menu-highlighter.js"></script>
+</head>
+<body class= "container">
+
+
+	<header>
+		<a href= "index.php">
+			<h1> Andrew Thankson </h1>
+			<img src= "images/andrew.jpeg" alt="Andrew">
+		</a>
+	</header>
+	<?php include "inc/nav-andrew.php"; ?>
+	<?php include "inc/nav.php"; ?>
+
+	<aside>
+		<ul>
+			<li>Computer Science Student, University of Rochester</li>
+			<li>Freelance Web Developer</li>
+			<li>Entrepreneur</li>
+		</ul>
+	</aside>
+
+
+	<main>
+		<p>I am currently a second-year student at the University of Rochester in Rochester, New York. I am thinking of majoring in Computer Science with a double minor in Philosophy and Psychology. I am originally from Ghana, where I grew up and schooled for most of my life. I moved to Rochester in the United States just a bit over a year ago to start my College Education. I founded a non-profit organization called Speak to the Nation Inc. in my home country Ghana to help kids from rural areas of the country learn more about the importance of Education by sharing personal experiences and also encouraging them through motivational messages and donating stationary to help them in their journey as students. I also started out as a freelance fronted web developer about a year or two ago. I have worked with a wide range of people in developing websites, applications, teaching students programming, and in other jobs such as being an office assistant and program assistant.
+ </p>
+	</main>
+
+
+	<?php include "inc/scripts.php";?>
+	<footer> <strong> CSC 174: Advanced Front-end Web Design and Development </strong></footer>
+	
+
+</body></html>
