259a0fabbf77753fcf23fc9f34318f751e195fed
mmanchen <mmanchen@u.rochester.edu>
2019-09-18 11:12:17 -0400

    Create ana-index.php

---
+++ ana-index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Ana Acuña </title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<script src="js/menu-highlighter.js"></script>
+</head>
+<body class= "container">
+
+
+	<header>
+		<a href= "index.php">
+			<h1> Ana Sofía Acuña </h1>
+			<img src= "images/ana.jpg" alt="Ana">
+		</a>
+	</header>
+	<?php include "inc/nav-ana.php"; ?>
+	<?php include "inc/nav.php"; ?>
+
+	<aside>
+		<ul>
+			<li>Marketing student flirting with Computer Science</li>
+			<li>Cat lover</li>
+			<li>Looking for the perfect first tattoo</li>
+		</ul>
+	</aside>
+
+
+	<main>
+		<p>My name is Maria Mancheno and I’m from Quito, Ecuador. I’m 21 years old and I currently go to the University of Rochester. My major is in Digital Media Studies and I’m also doing a minor in Math. My hobbies include playing soccer, playing the guitar and reading. I work as a social media manager and web developer at the Peak Lab in the University of Rochester Medical Center. This last summer I had the opportunity to do an internship in an Ecuadorian firm where I was able to improve my programming skills. My favorite thing is to travel around the world to learn about new cultures and meet new interesting people. 
+ </p>
+	</main>
+
+
+	<?php include "inc/scripts.php";?>
+	<footer> <strong> CSC 174: Advanced Front-end Web Design and Development </strong></footer>
+	
+
+</body></html>
