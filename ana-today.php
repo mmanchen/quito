587dc2f80252ad6c60519ade20bfdd51f81f710c
mmanchen <mmanchen@u.rochester.edu>
2019-09-18 11:12:21 -0400

    Create ana-today.php

---
+++ ana-today.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Ana Acuña </title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<script src="js/menu-highlighter.js"></script>
+</head>
+<body class= "container">
+
+
+	<header>
+		<a href= "index.php">
+			<h1> Ana Sofía Acuña </h1>
+			<img src= "images/ana.jpg" alt="Ana">
+		</a>
+	</header>
+	<?php include "inc/nav-ana.php"; ?>
+	<?php include "inc/nav.php"; ?>
+
+	<aside>
+		<ul>
+			<li>Marketing student flirting with Computer Science</li>
+			<li>Cat lover</li>
+			<li>Looking for the perfect first tattoo</li>
+		</ul>
+	</aside>
+
+
+	<main>
+		<h1> Today </h1>
+
+		<p>When I’m not in the classroom or in the library, I’m either running around doing something for my sorority, Gamma Phi Beta. I’m currently the President and I was formerly the Financial Vice President so I guess you could say I’m invested in my sorority. I just want to do the best I can because I’ve gotten to know most of my friends through it! It’s also been a great avenue to do things in the community because I feel like it’s important to get out and get to know the real Rochester if I’m going to say I lived here for four years. That’s why I’ve also dabbled in community service programs here. Recently I joined a pen pal program hosted by the Student Association, and joined the executive aid committee to help plan the program. I’m also a member of Student Programming Board and that’s really fun because we get to plan huge events like concerts for students of the U of R.</p>
+
+	</main>
+
+
+	<?php include "inc/scripts.php";?>
+	<footer> <strong> CSC 174: Advanced Front-end Web Design and Development </strong></footer>
+	
+
+</body></html>
