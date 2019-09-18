416f8247837bca9df07d6f3ddc9f8618a4f5f8e1
mmanchen <mmanchen@u.rochester.edu>
2019-09-18 11:13:24 -0400

    Delete navn.php

--- inc/navn.php
+++
@@ -1,9 +0,0 @@
-
-	<nav>
-		<ul>
-			<li><a <?php if($current == 'home') {echo 'class="is-current"';} ?> href="andrew.php">Me</a></li>
-			<li><a <?php if($current == 'earlydays') {echo 'class="is-current"';} ?> href="early_days.php">Early Days</a></li>
-			<li><a <?php if($current == 'college') {echo 'class="is-current"';} ?> href="college.php">College Life</a><li>
-			<li><a <?php if($current == 'future') {echo 'class="is-current"';} ?> href="future.php">Future Plans</a></li>
-		</ul>
-	</nav>
