3867538771b5b59b3d9b17fb67af8ba69b460774
mmanchen <mmanchen@u.rochester.edu>
2019-09-18 11:17:48 -0400

    Update nav-clara.php

--- inc/nav-clara.php
+++ inc/nav-clara.php
@@ -2,7 +2,7 @@
 	<ul>
 			<li><a href="clara-index.php">Me</a></li>
 			<li><a href="clara-favorite.php">Favorite Things</a></li>
-			<li><a href="clara-lifestyle.php">Lifestyle</a></li>
+			<li><a href="clara-famous.php">Famous Lifestyle</a></li>
 			<li><a href="clara-past.php">Past</a></li>
 	</ul>	
 	</nav>
