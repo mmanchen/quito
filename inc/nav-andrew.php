d5ab915d3fed40aeb774bda03b2b0f33da1b7536
mmanchen <mmanchen@u.rochester.edu>
2019-09-18 11:13:12 -0400

    Create nav-andrew.php

---
+++ inc/nav-andrew.php
@@ -0,0 +1,16 @@
+	<nav class="sub-menus">
+	<ul>
+		<li>
+			<a href="andrew-index.php"> Me </a>
+		</li>
+		<li>
+			<a href="andrew-early.php">Early Days</a>
+		</li>
+		<li>
+			<a href="andrew-college.php">College Life</a>
+		</li>
+		<li>
+			<a href="andrew-future.php"> Future Plans </a>
+		</li>
+	</ul>	
+	</nav>
