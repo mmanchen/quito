de9613db26edc0ba48ac044ba4f3b8170d2b3cc5
mmanchen <mmanchen@u.rochester.edu>
2019-09-22 17:52:11 -0400

    Update ana-journey.php

--- ana-journey.php
+++ ana-journey.php
@@ -29,8 +29,7 @@
 
 	<main>
 		<h1> Journey </h1>
-
-		<p>As much as I love my hometown, I followed my brother’s footsteps and went to school far from home for college. While my brother chose to study in the Midwest, I chose the Northeast which is just as cold. Adjusting to college was hard because I wasn’t quite sure what I wanted to study, although I pretended I was going to be a hardcore business major. After some experimenting in Data Science and Epidemiology, I decided I indeed want to study Business, specifically Marketing. I really liked my Statistics courses, but I liked getting creative with a group of peers more than anything. Marketing just feels like the best fit right now! I’m also minoring in Computer Science because it sparked my interest in my journey to figuring out my interests, but not enough to major in it. Besides, I would rather dabble in different CS courses and have fun with it than overstress about it since it’s not where my passions lie. </p>
+		<p>As much as I love my hometown, I followed my brother’s footsteps and went to school far from home for college. While my brother chose to study in the Midwest, I chose the Northeast which is just as cold. Adjusting to college was hard because I wasn’t quite sure what I wanted to study, although I pretended I was going to be a hardcore business major. After some experimenting in Data Science and Epidemiology, I decided I indeed want to study Business, specifically Marketing. I really liked my Statistics courses, but I liked getting creative with a group of peers more than anything. Marketing just feels like the best fit right now! I’m also minoring in Computer Science because it sparked my interest in my journey to figuring out my interests, but not enough to major in it. Besides, I would rather dabble in different CS courses and have fun with it than overstress about it since it’s not where my passions lie.</p>
 
 	</main>
 
